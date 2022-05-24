1455cecfa4570993eb406a2ad8bda05e5f5e1091
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-05-24 17:34:33 +0400

    react hook form added

--- src/Pages/Authentication/Register.tsx
+++ src/Pages/Authentication/Register.tsx
@@ -1,10 +1,29 @@
-import { Box, Grid, Stack, TextField, Typography } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
+import {
+  useCreateUserWithEmailAndPassword,
+  useUpdateProfile,
+} from "react-firebase-hooks/auth";
 import { useContext, useState } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { Context } from "../../App";
 import "./input.css";
 
 export const Register = () => {
+  // const [createUserWithEmailAndPassword, user, loading, error] =
+  //   useCreateUserWithEmailAndPassword(auth);
+  // const [updateProfile] = useUpdateProfile(auth);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    watch,
+    getValues,
+  } = useForm({
+    mode: "onTouched",
+  });
+  watch("password", "");
   const [value, setValue] = useState("");
   const value1 = useContext(Context);
 
@@ -16,7 +35,6 @@ export const Register = () => {
   let color: string;
   let color2: string;
   let color3: string;
-  let color4: string;
   if (theme === "dark") {
     backgroundColor = "#111827";
     color = "#e11d48";
@@ -29,101 +47,199 @@ export const Register = () => {
     color3 = "#000";
   }
 
+  const onSubmit = async (data: any) => {
+    // await createUserWithEmailAndPassword(data.email, data.password);
+    // await updateProfile({ displayName: data.name });
+    console.log(data);
+  };
+
   return (
-    <Stack mt={12}>
-      <Typography
-        variant="h4"
-        textAlign="center"
-        sx={{ fontWeight: 800 }}
-        mb={3}
-      >
-        Register
-      </Typography>
-      <Stack direction="row" spacing={2} mx="auto" mb={2}>
-        <Grid
-          container
-          columnSpacing={2}
-          rowSpacing={2}
-          sx={{ textAlign: "center" }}
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack mt={12}>
+        <Typography
+          variant="h4"
+          textAlign="center"
+          sx={{ fontWeight: 800 }}
+          mb={3}
         >
-          <Grid item xs={12} sm={6}>
-            <Box>
+          Register
+        </Typography>
+        <Stack direction="row" spacing={2} mx="auto" mb={2}>
+          <Grid
+            container
+            columnSpacing={2}
+            rowSpacing={2}
+            sx={{ textAlign: "center" }}
+          >
+            <Grid item xs={12} sm={6}>
+              <Box>
+                <input
+                  type="text"
+                  style={{
+                    backgroundColor,
+                    color: color3,
+                    borderBottom: `2px solid ${color}`,
+                  }}
+                  placeholder="Your Name"
+                  {...register("name", {
+                    required: {
+                      value: true,
+                      message: "Name is Required",
+                    },
+                  })}
+                ></input>
+                {errors.name?.type === "required" && (
+                  <Typography variant="body2" color="error">
+                    {errors.name?.message}
+                  </Typography>
+                )}
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Box>
+                <input
+                  type="email"
+                  style={{
+                    backgroundColor,
+                    color: color3,
+                    borderBottom: `2px solid ${color}`,
+                  }}
+                  placeholder="Your Email"
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: "Email is Required",
+                    },
+                    pattern: {
+                      value: /^\S+@\S+\.\S+$/,
+                      message: "Provide a valid email",
+                    },
+                  })}
+                ></input>
+                {errors.email?.type === "required" && (
+                  <Typography variant="body2" color="error">
+                    {errors.email?.message}
+                  </Typography>
+                )}
+                {errors.email?.type === "pattern" && (
+                  <Typography variant="body2" color="error">
+                    {errors.email?.message}
+                  </Typography>
+                )}
+              </Box>
+            </Grid>
+          </Grid>
+        </Stack>
+        <Stack direction="row" spacing={2} mx="auto">
+          <Grid
+            container
+            columnSpacing={2}
+            rowSpacing={2}
+            sx={{ textAlign: "center" }}
+          >
+            <Grid item xs={12} sm={6}>
               <input
-                type="text"
+                type="password"
                 style={{
                   backgroundColor,
                   color: color3,
                   borderBottom: `2px solid ${color}`,
                 }}
-                placeholder="Your Name"
+                placeholder="Password"
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "Password is Required",
+                  },
+                  pattern: {
+                    value: /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{6,20})/,
+                    message:
+                      "Must contain at least one digit,lowercase,special character, min 6 & max 20 characters",
+                  },
+                })}
               ></input>
-            </Box>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Box>
+              {errors.password?.type === "required" && (
+                <Typography variant="body2" color="error">
+                  {errors.password?.message}
+                </Typography>
+              )}
+              {errors.password?.type === "pattern" && (
+                <Typography
+                  sx={{ fontSize: "10px" }}
+                  variant="body2"
+                  color="error"
+                >
+                  {errors.password?.message.slice(0, 50)}
+                </Typography>
+              )}
+              {errors.password?.type === "pattern" && (
+                <Typography
+                  sx={{ fontSize: "10px" }}
+                  variant="body2"
+                  color="error"
+                >
+                  {errors.password?.message.slice(50, 100)}
+                </Typography>
+              )}
+            </Grid>
+            <Grid item xs={12} sm={6}>
               <input
-                type="email"
+                type="password"
                 style={{
                   backgroundColor,
                   color: color3,
                   borderBottom: `2px solid ${color}`,
                 }}
-                placeholder="Your Email"
+                placeholder="Confirm Password"
+                {...register("confirm_password", {
+                  required: {
+                    value: true,
+                    message: "Confrim Password is Required",
+                  },
+                  validate: (val: string) => {
+                    if (watch("password") !== val) {
+                      return "Your passwords do not match";
+                    }
+                  },
+                })}
               ></input>
-            </Box>
+              {errors.confirm_password?.type === "required" && (
+                <Typography variant="body2" color="error">
+                  {errors.confirm_password?.message}
+                </Typography>
+              )}
+              {errors.confirm_password?.type === "validate" && (
+                <Typography variant="body2" color="error">
+                  {errors.confirm_password?.message}
+                </Typography>
+              )}
+            </Grid>
           </Grid>
-        </Grid>
+        </Stack>
+        <input style={{ margin: "0px auto", marginTop: "10px" }} type="file" />
+        <Typography variant="body1" textAlign="center">
+          Already have an account?
+          <Link
+            style={{ textDecoration: "none", marginLeft: "5px" }}
+            to="/login"
+          >
+            Please login
+          </Link>
+        </Typography>
+        <input
+          style={{
+            margin: "10px auto",
+            backgroundColor,
+            padding: "12px 20px",
+            borderRadius: "5px",
+            color: color3,
+            cursor: "pointer",
+            border: "none",
+          }}
+          type="submit"
+          value="Register"
+        />
       </Stack>
-      <Stack direction="row" spacing={2} mx="auto">
-        <Grid
-          container
-          columnSpacing={2}
-          rowSpacing={2}
-          sx={{ textAlign: "center" }}
-        >
-          <Grid item xs={12} sm={6}>
-            <input
-              type="password"
-              style={{
-                backgroundColor,
-                color: color3,
-                borderBottom: `2px solid ${color}`,
-              }}
-              placeholder="Password"
-            ></input>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <input
-              type="password"
-              style={{
-                backgroundColor,
-                color: color3,
-                borderBottom: `2px solid ${color}`,
-              }}
-              placeholder="Confirm Password"
-            ></input>
-          </Grid>
-        </Grid>
-      </Stack>
-      <Typography variant="body1" textAlign="center" mt={2}>
-        Already have an account?
-        <Link style={{ textDecoration: "none", marginLeft: "5px" }} to="/login">
-          Please login
-        </Link>
-      </Typography>
-      <input
-        style={{
-          margin: "10px auto",
-          backgroundColor,
-          padding: "12px 20px",
-          borderRadius: "5px",
-          color: color3,
-          cursor: "pointer",
-          border: "none",
-        }}
-        type="submit"
-        value="Register"
-      />
-    </Stack>
+    </form>
   );
 };
